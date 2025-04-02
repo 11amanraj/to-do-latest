64ef57c71f0e0805bc75b3337883f311f526f9ce
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-03 01:40:15 +0530

    Added completing and deleting todo functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState, useRef } from "react";
+import Image from "next/image";
 
 interface todo {
   id: string,
@@ -41,18 +42,33 @@ export default function Home() {
     }
   }
 
-  const completeTodoHandler = () => {}
-  const deleteTodoHandler = () => {}
+  const completeTodoHandler = (id: string) => {
+    const updatedTodos = todos.map(todo => {
+      if(todo.id === id) {
+        return {...todo, completed: !todo.completed}
+      } else return todo
+    })
+    setTodos([...updatedTodos])
+  }
+
+  const deleteTodoHandler = (id: string) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id)
+    setTodos([...updatedTodos])
+  }
 
   return (
-    <div>
+    <div className="mx-6">
       <form onSubmit={addTodoHandler}>
         <input ref={inputRef} type="text" placeholder="Create a new todo..."/>
       </form>
       <ul>
         {todos.map(todo => {
           return (
-            <li className="bg-red-500" key={todo.id}>{todo.text}</li>
+            <li className="flex gap-3 px-4 py-5 text-3 bg-red-800" key={todo.id}>
+              <button className="w-5 h-5 rounded-full bg-green-50 flex justify-center items-center"><Image onClick={() => completeTodoHandler(todo.id)} src="/icon-check.svg" alt="check" width='12' height='12'/></button>
+              <p className="flex-1">{todo.text}</p>
+              <button><Image onClick={() => deleteTodoHandler(todo.id)} src="/icon-cross.svg" alt="delete" width='12' height='12'/></button>
+            </li>
           )
         })}
       </ul>
