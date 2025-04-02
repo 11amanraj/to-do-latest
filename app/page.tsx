97c30c960a3be264b27def08918371455f7d0dfe
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-03 01:44:15 +0530

    Added functionality to filter todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import Image from "next/image";
 
 interface todo {
@@ -8,6 +8,8 @@ interface todo {
   text: string,
   completed: boolean,
 }
+
+type FilterOptions = "all" | "active" | "completed";
 
 export default function Home() {
   const inputRef = useRef<HTMLInputElement>(null)
@@ -28,6 +30,22 @@ export default function Home() {
       completed: false
     }
   ])
+  const [isShowing, setIsShowing] = useState<FilterOptions>('all')
+  const [filteredTodos, setFilteredTodos] = useState<todo[]>([])
+
+  useEffect(() => {
+    let filteredTodos: todo[] = []
+    if (isShowing == 'all') {
+      filteredTodos = [...todos]
+    } else if (isShowing == 'active') {
+      const newTodos = todos.filter(todo => !todo.completed)
+      filteredTodos = [...newTodos]
+    } else {
+      const newTodos = todos.filter(todo => todo.completed)
+      filteredTodos = [...newTodos]
+    }
+    setFilteredTodos(filteredTodos);
+  }, [isShowing, todos])
 
   const addTodoHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -62,7 +80,7 @@ export default function Home() {
         <input ref={inputRef} type="text" placeholder="Create a new todo..."/>
       </form>
       <ul>
-        {todos.map(todo => {
+        {filteredTodos.map(todo => {
           return (
             <li className="flex gap-3 px-4 py-5 text-3 bg-red-800" key={todo.id}>
               <button className="w-5 h-5 rounded-full bg-green-50 flex justify-center items-center"><Image onClick={() => completeTodoHandler(todo.id)} src="/icon-check.svg" alt="check" width='12' height='12'/></button>
@@ -72,6 +90,11 @@ export default function Home() {
           )
         })}
       </ul>
+      <div className="filter">
+        <button onClick={() => setIsShowing('all')}>All</button>
+        <button onClick={() => setIsShowing('active')}>Active</button>
+        <button onClick={() => setIsShowing('completed')}>Completed</button>
+      </div>
     </div>
   );
 }
