c20ef0a0ba4ef76448162fcd1fe27ae79461c23c
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-03 01:24:52 +0530

    added todo list element

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
+'use client'
+
+import { useState } from "react";
+
+interface todo {
+  id: string,
+  text: string,
+  completed: boolean,
+}
+
 export default function Home() {
+  const [todos, setTodos] = useState<todo[]>([
+    {
+      id: Math.random().toString(),
+      text: 'First Todo',
+      completed: false
+    },
+    {
+      id: Math.random().toString(),
+      text: 'Second Todo',
+      completed: true
+    },
+    {
+      id: Math.random().toString(),
+      text: 'Third Todo',
+      completed: false
+    }
+  ])
+
+  const addTodoHandler = () => {}
+  const completeTodoHandler = () => {}
+  const deleteTodoHandler = () => {}
+
   return (
     <div>
-      This is working!
+      <ul>
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>{todo.text}</li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
