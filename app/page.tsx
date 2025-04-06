6b6720d86634a5122d3ee545acc0dd7fcbbb4afb
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-06 19:13:16 +0530

    Added message for empty todo list

--- app/page.tsx
+++ app/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
       <div className="px-6 pb-10 flex flex-col dark:bg-[url('/bg-mobile-dark.jpg')] bg-[url('/bg-mobile-light.jpg')] sm:bg-[url('/bg-desktop-light.jpg')] sm:dark:bg-[url('/bg-desktop-dark.jpg')] bg-cover">
         <div className="sm:w-150 md:w-180 lg:w-240 sm:self-center flex justify-between items-center mt-12 mb-5 ">
           <p className="text-3xl md:text-5xl">TODO</p>
-          <div>Dark Mode</div>
+          {/* <div>Dark Mode</div> */}
         </div>
         <form className="sm:w-150 md:w-180 lg:w-240 sm:self-center flex gap-3 px-4 py-5 bg-foreground rounded-sm" onSubmit={addTodoHandler}>
         <button className="w-5 h-5 rounded-full border-[1px] border-primary border-opacity-50 flex justify-center items-center"></button>          <input ref={inputRef} type="text" placeholder="Create a new todo..."/>
@@ -87,23 +87,23 @@ export default function Home() {
       </div>
       <div className="-mt-6 px-6 sm:px-0 flex flex-col gap-4 sm:w-150 md:w-180 lg:w-240 sm:self-center">
         <ul className="rounded-sm bg-foreground divide-y">
-          {filteredTodos.map(todo => {
+          {filteredTodos.length == 0 ? <div  className="text-2xl px-4 py-5">Add a todo. It’s like writing a diary, but with deadlines and guilt.</div> : filteredTodos.map(todo => {
             return (
               <li className="flex gap-3 px-4 py-5 text-3" key={todo.id}>
                 {todo.completed 
-                  ? <button onClick={() => completeTodoHandler(todo.id)} className="w-5 h-5 rounded-full bg-green-500 flex justify-center items-center"><Image src="/icon-check.svg" alt="check" width='12' height='12'/></button>
-                  : <button onClick={() => completeTodoHandler(todo.id)} className="w-5 h-5 rounded-full border-[1px] border-primary border-opacity-50 flex justify-center items-center"></button>
+                  ? <button onClick={() => completeTodoHandler(todo.id)} className="cursor-pointer w-5 h-5 rounded-full bg-green-500 flex justify-center items-center"><Image src="/icon-check.svg" alt="check" width='12' height='12'/></button>
+                  : <button onClick={() => completeTodoHandler(todo.id)} className="cursor-pointer w-5 h-5 rounded-full border-[1px] border-primary border-opacity-50 flex justify-center items-center"></button>
                 }
-                <p className="flex-1 ">{todo.text}</p>
-                <button><Image onClick={() => deleteTodoHandler(todo.id)} src="/icon-cross.svg" alt="delete" width='12' height='12'/></button>
+                <p className="flex-1">{todo.text}</p>
+                <button className="cursor-pointer"><Image onClick={() => deleteTodoHandler(todo.id)} src="/icon-cross.svg" alt="delete" width='12' height='12'/></button>
               </li>
             )
           })}
         </ul>
         <div className="bg-foreground px-4 py-5 rounded-sm flex gap-4 justify-center">
-          <button onClick={() => setIsShowing('all')}>All</button>
-          <button onClick={() => setIsShowing('active')}>Active</button>
-          <button onClick={() => setIsShowing('completed')}>Completed</button>
+          <button className="cursor-pointer" onClick={() => setIsShowing('all')}>All</button>
+          <button className="cursor-pointer" onClick={() => setIsShowing('active')}>Active</button>
+          <button className="cursor-pointer" onClick={() => setIsShowing('completed')}>Completed</button>
         </div>
       </div>
     </div>
